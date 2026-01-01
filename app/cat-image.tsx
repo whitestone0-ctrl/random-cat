e3f208b0a865d56880990f99e3cb58e6c040b593
whitestone0 <whitestone0@gmail.com>
2026-01-02 00:29:05 +0900

    initial commit

---
+++ app/cat-image.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState } from "react";
+import { fetchImage } from "./fetch-image";
+import styles from "./page.module.css";
+
+// コンポーネントの引数を定義する
+type CatImageProps = {
+    url: string;
+};
+
+// 画像を表示するコンポーネント
+export function CatImage({ url }: CatImageProps) {
+    const [imageUrl, setImageUrl] = useState(url);
+
+    const refreshImage = async () => {
+        setImageUrl("");
+        const image = await fetchImage();
+        setImageUrl(image.url);
+    };
+
+    return (
+        <div className={styles.page}>
+            <button onClick={refreshImage} className={styles.button}>画像を変更する</button>
+            <div className={styles.frame}>
+                {imageUrl && <img src={imageUrl} />}
+            </div>
+        </div>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { connection } from "next/server";
+import { fetchImage } from "./fetch-image";
+import { CatImage } from "./cat-image";
+
+export default async function Home() {
+  await connection();
+  const image = await fetchImage();
+  console.log("Home: 画像情報を取得しました", image);
+  return <CatImage url={image.url} />;
+}
